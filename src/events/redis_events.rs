6ab4b9a3f0d0e50d518e6b6c2b6c247ce6fffcc3
Spencer Najib <spencernajib2@gmail.com>
2025-09-12 13:45:58 +0100

    add queue-stream-event

---
+++ src/events/redis_events.rs
@@ -0,0 +1,75 @@
+use serde::de::{value, DeserializeOwned};
+
+use crate::{JobState, KioError, KioResult};
+use derive_more::Debug;
+use std::str::FromStr;
+#[derive(Debug)]
+pub struct QueueStreamEvent<R, P> {
+    id: String,
+    event: JobState,
+    delay: Option<u64>,
+    prev: Option<JobState>,
+    job_id: String,
+    #[debug(skip)]
+    retuned_value: Option<R>,
+    #[debug(skip)]
+    progress_data: Option<P>,
+    name: Option<String>,
+}
+impl<R, P> Default for QueueStreamEvent<R, P> {
+    fn default() -> Self {
+        Self {
+            id: Default::default(),
+            delay: None,
+            event: Default::default(),
+            prev: Default::default(),
+            job_id: Default::default(),
+            retuned_value: None,
+            name: Default::default(),
+            progress_data: None,
+        }
+    }
+}
+use redis::{
+    streams::{StreamId, StreamReadReply},
+    FromRedisValue,
+};
+impl<R: DeserializeOwned, P: DeserializeOwned> QueueStreamEvent<R, P> {
+    pub fn from_stream_read_reply(events_key: &str, reply: StreamReadReply) -> Vec<Self> {
+        if let Some(keyed_events) = reply.keys.iter().find(|event| event.key == events_key) {
+            let events = keyed_events.ids.iter().flat_map(Self::try_from).collect();
+            return events;
+        }
+        vec![]
+    }
+}
+impl<R: DeserializeOwned, P: DeserializeOwned> TryFrom<&StreamId> for QueueStreamEvent<R, P> {
+    type Error = KioError;
+
+    fn try_from(value: &StreamId) -> Result<Self, Self::Error> {
+        let mut event = Self {
+            id: value.id.to_string(),
+            ..Default::default()
+        };
+        for (key, val) in &value.map {
+            let val_str = String::from_redis_value(val)?;
+            match key.to_lowercase().as_str() {
+                "job_id" => event.job_id = val_str,
+                "name" => event.name = Some(val_str),
+                "delay" => event.delay = serde_json::from_str(&val_str)?,
+                "data" => event.progress_data = serde_json::from_str(&val_str)?,
+
+                "returnedvalue" => event.retuned_value = serde_json::from_str(&val_str)?,
+
+                "event" => {
+                    let parsed = JobState::from_str(&val_str).map_err(std::io::Error::other)?;
+                    event.event = parsed;
+                }
+                "prev" => event.prev = JobState::from_str(&val_str).ok(),
+                _ => { /* Ignore unknown fields if your hash might contain others */ }
+            }
+        }
+
+        Ok(event)
+    }
+}
