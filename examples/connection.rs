1f2bac8299eb4458b58be80ad67f8d433bcb9c94
Spencer Najib <spencernajib2@gmail.com>
2025-09-16 15:47:58 +0100

    update example to support all job features: delaying, prioritization

--- examples/connection.rs
+++ examples/connection.rs
@@ -3,6 +3,7 @@ use kio_mq::{
     fetch_redis_pass, framed, EventParameters, Job, JobOptions, KioResult, Queue, Worker,
     WorkerOpts,
 };
+use tokio_util::sync::CancellationToken;
 use uuid::Uuid;
 #[tokio::main]
 #[framed]
@@ -13,28 +14,12 @@ async fn main() -> KioResult<()> {
         cfg.redis.password = password;
     }
     let queue = Queue::<String, String, i32>::new(None, "trial", &config, None).await?;
-
-    let count = 10;
-    for i in 0..count {
-        let job_opts = JobOptions {
-            delay: 100 * i as u64,
-            ..Default::default()
-        };
-        let name = Uuid::new_v4().to_string();
-        let _ = queue
-            .add_job(&name, "data".to_lowercase(), Some(job_opts))
-            .await?;
-    }
-    let opts = WorkerOpts {
-        //concurrency: 1,
-        ..Default::default()
-    };
-    let last_job_id = queue.current_jobs();
-    let processor = |con: _, job: Job<_, _, _>| process_callback(con, job);
-    let worker = Worker::new(&queue, processor, Some(opts))?;
-    let cancel = worker.cancellation_token.clone();
+    let token = CancellationToken::default();
+    let cancel = token.clone();
+    let metrics = queue.current_metrics.clone();
     let event_listener = move |state: _| {
         let cancel = cancel.clone();
+        let metrics = metrics.clone();
         async move {
             // do something with return state
             if let EventParameters::Completed {
@@ -44,13 +29,14 @@ async fn main() -> KioResult<()> {
             } = state
             {
                 let diff = (job.processed_on.unwrap_or_default() - job.ts).num_milliseconds();
-                let id = last_job_id.to_string();
-                if job.id.as_ref().unwrap().contains(&id) {
-                    //println!("finished in {:#?}: delay_ms: {diff}", now.elapsed());
+                let ran_time = (job.finished_on.unwrap_or_default()
+                    - job.processed_on.unwrap_or_default())
+                .num_milliseconds();
+                if metrics.all_jobs_completed() {
                     cancel.cancel();
                 }
                 println!(
-                    "finished job {} delayed for {} ms, expected_delay: {}",
+                    "finished job  {}  ran for {ran_time} ms with an actual delay of  {} ms and  expected_delay: {}",
                     job.id.unwrap_or_default(),
                     diff,
                     job.opts.delay,
@@ -59,6 +45,31 @@ async fn main() -> KioResult<()> {
         }
     };
     queue.on_all_events(event_listener).await;
+
+    let count = 10;
+    for _i in 0..count {
+        //use rand::Rng;
+        //let priority = rand::rng().random_range(1..count); // ucomment to use  random priority
+
+        //let priority = count - _i; // ucomment to use a priority of count - index (job_id -1)
+        let job_opts = JobOptions {
+            //delay: 200 * _i as u64, // uncomment to add delay
+            //priority, // uncomment to set priority
+            ..Default::default()
+        };
+        let name = Uuid::new_v4().to_string();
+        let _ = queue
+            .add_job(&name, "data".to_lowercase(), Some(job_opts))
+            .await?;
+    }
+    let opts = WorkerOpts {
+        //concurrency: 1, // uncomment to use set concurrency
+        ..Default::default()
+    };
+    let processor = |con: _, job: Job<_, _, _>| process_callback(con, job);
+    let mut worker = Worker::new(&queue, processor, Some(opts))?;
+    worker.cancellation_token = token;
+
     worker.run()?;
 
     while worker.is_running() {} // do nothing
