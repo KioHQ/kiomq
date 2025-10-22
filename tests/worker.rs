11c936665ee097bc08f50d241859f9e8271bb015
Spencer Najib <spencernajib2@gmail.com>
2025-10-23 00:16:50 +0100

    use redis instead of keydb in ci try buildjet wip: improve worker and fix them in ci

--- tests/worker.rs
+++ tests/worker.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod worker {
+    use crossbeam_queue::ArrayQueue;
     use kio_mq::{fetch_redis_pass, EventParameters, Job, JobOptions, KioError, QueueEventMode};
     use kio_mq::{Config, KioResult, Queue, QueueOpts, Worker};
     use std::collections::VecDeque;
@@ -15,12 +16,31 @@ mod worker {
     });
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn run_jobs_to_completion() -> KioResult<()> {
+    async fn runs_jobs_to_completion() -> KioResult<()> {
         let config = &CONFIG;
         let queue_opts = QueueOpts::default();
         let name = Uuid::new_v4().to_string();
         let queue = Queue::<i32, i32, i32>::new(None, &name, config, Some(queue_opts)).await?;
-        let job_iterator = (0..4).map(|i| (i.to_string(), None, i));
+        let count = 4;
+        let completed: Arc<ArrayQueue<Job<_, _, _>>> = Arc::new(ArrayQueue::new(count as usize));
+        let jobs = completed.clone();
+        queue.on(
+            kio_mq::JobState::Completed,
+            move |state: kio_mq::EventParameters<i32, i32, i32>| {
+                let completed = jobs.clone();
+                async move {
+                    if let EventParameters::Completed {
+                        job,
+                        prev_state: _,
+                        result: _,
+                    } = state
+                    {
+                        let _ = completed.push(job);
+                    }
+                }
+            },
+        );
+        let job_iterator = (0..count).map(|i| (i.to_string(), None, i));
         let processor = move |_conn, job: kio_mq::Job<i32, i32, i32>| async move {
             Ok::<i32, KioError>(job.data.unwrap())
         };
@@ -29,10 +49,10 @@ mod worker {
         assert!(worker.is_running());
         let jobs = queue.bulk_add(job_iterator).await?;
         // wait for metrics to update
-        while !queue.get_metrics().await?.all_jobs_completed() {}
+        while completed.len() != count as usize {}
         worker.close(false);
         assert!(!worker.is_running());
-        let metrics = queue.current_metrics.as_ref();
+        let metrics = queue.get_metrics().await?;
         assert_eq!(
             metrics.waiting.load(std::sync::atomic::Ordering::Acquire),
             0,
@@ -67,7 +87,7 @@ mod worker {
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn run_delayed_jobs() -> KioResult<()> {
+    async fn runs_delayed_jobs() -> KioResult<()> {
         use crossbeam_queue::ArrayQueue;
         let config = &CONFIG;
         let queue_opts = QueueOpts {
@@ -87,16 +107,11 @@ mod worker {
         let completed: Arc<ArrayQueue<Job<_, _, _>>> = Arc::new(ArrayQueue::new(count as usize));
         let jobs = completed.clone();
         queue.on(
-            kio_mq::JobState::Completed,
+            kio_mq::JobState::Active,
             move |state: kio_mq::EventParameters<i32, i32, i32>| {
                 let completed = jobs.clone();
                 async move {
-                    if let EventParameters::Completed {
-                        job,
-                        prev_state: _,
-                        result: _,
-                    } = state
-                    {
+                    if let EventParameters::Active { job, prev_state: _ } = state {
                         let _ = completed.push(job);
                     }
                 }
@@ -107,10 +122,9 @@ mod worker {
         };
         let worker = Worker::new_async(&queue, processor, None)?;
         worker.run()?;
-        let jobs = queue.bulk_add(job_iterator).await?;
+        let _jobs = queue.bulk_add(job_iterator).await?;
         assert!(worker.is_running());
-        // wait for metrics to update
-        while !queue.get_metrics().await?.all_jobs_completed() {}
+        while completed.len() != count as usize {}
         worker.close(false);
         assert!(!worker.is_running());
         assert_eq!(completed.len(), count as usize);
@@ -118,10 +132,6 @@ mod worker {
         assert_eq!(
             metrics.delayed.load(std::sync::atomic::Ordering::Acquire),
             0,
-        );
-        assert_eq!(
-            metrics.completed.load(std::sync::atomic::Ordering::Acquire),
-            jobs.len() as u64,
         );
         queue.obliterate().await?;
         while let Some(job) = completed.pop() {
@@ -139,10 +149,32 @@ mod worker {
     async fn pauses_and_resumes_when_queue_is_idle() -> KioResult<()> {
         use std::time::Duration;
         let config = &CONFIG;
-        let queue_opts = QueueOpts::default();
+        let queue_opts = QueueOpts {
+            event_mode: Some(QueueEventMode::PubSub),
+            ..Default::default()
+        };
         let name = Uuid::new_v4().to_string();
         let queue = Queue::<i32, i32, i32>::new(None, &name, config, Some(queue_opts)).await?;
-        let job_iterator = (0..4).map(|i| (i.to_string(), None, i));
+        let count = 4;
+        let completed: Arc<ArrayQueue<Job<_, _, _>>> = Arc::new(ArrayQueue::new(count as usize));
+        let jobs = completed.clone();
+        queue.on(
+            kio_mq::JobState::Completed,
+            move |state: kio_mq::EventParameters<i32, i32, i32>| {
+                let completed = jobs.clone();
+                async move {
+                    if let EventParameters::Completed {
+                        job,
+                        prev_state: _,
+                        result: _,
+                    } = state
+                    {
+                        let _ = completed.push(job);
+                    }
+                }
+            },
+        );
+        let job_iterator = (0..count).map(|i| (i.to_string(), None, i));
         let processor = move |_conn, job: kio_mq::Job<i32, i32, i32>| async move {
             Ok::<i32, KioError>(job.data.unwrap())
         };
@@ -151,25 +183,25 @@ mod worker {
         assert!(worker.is_running());
         queue.bulk_add(job_iterator).await?;
         // wait for all previous to complete
-        while !queue.get_metrics().await?.all_jobs_completed() {}
+        while completed.len() != count as usize {}
         tokio::time::sleep(Duration::from_millis(100)).await;
         assert!(worker.is_idle());
         queue.add_job("test", 1, None).await?;
         // wait for new job to get picked up
-        while queue.get_metrics().await?.queue_has_work() {}
+        while worker.is_idle() {}
         assert!(!worker.is_idle());
         assert!(worker.is_running());
         let metrics = queue.current_metrics.as_ref();
         assert_eq!(
             metrics.waiting.load(std::sync::atomic::Ordering::Acquire),
-            0,
+            1,
         );
         queue.obliterate().await?;
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn run_prioritized_jobs_correctly() -> KioResult<()> {
+    async fn runs_prioritized_jobs_correctly() -> KioResult<()> {
         use crossbeam_queue::ArrayQueue;
         let config = &CONFIG;
         let queue_opts = QueueOpts::default();
@@ -183,19 +215,14 @@ mod worker {
             };
             (i.to_string(), Some(job_opts), i)
         });
-        let completed: Arc<ArrayQueue<u64>> = Arc::new(ArrayQueue::new(count as usize));
-        let jobs = completed.clone();
+        let moved_to_active: Arc<ArrayQueue<u64>> = Arc::new(ArrayQueue::new(count as usize));
+        let jobs = moved_to_active.clone();
         queue.on(
-            kio_mq::JobState::Completed,
+            kio_mq::JobState::Active,
             move |state: kio_mq::EventParameters<i32, i32, i32>| {
                 let completed = jobs.clone();
                 async move {
-                    if let EventParameters::Completed {
-                        job,
-                        prev_state: _,
-                        result: _,
-                    } = state
-                    {
+                    if let EventParameters::Active { job, prev_state: _ } = state {
                         let id = job.id.unwrap();
                         let _ = completed.push(id);
                     }
@@ -207,28 +234,95 @@ mod worker {
         };
         let worker = Worker::new_async(&queue, processor, None)?;
         worker.run()?;
-        let mut jobs = queue.bulk_add(job_iterator).await?;
+        let jobs = queue.bulk_add(job_iterator).await?;
         // wait for metrics to update
-        while !queue.get_metrics().await?.all_jobs_completed() {}
-        assert_eq!(completed.len(), count as usize);
+        //tokio::time::slee
+        while moved_to_active.len() < count as usize {}
+        assert_eq!(moved_to_active.len(), count as usize);
         let metrics = queue.current_metrics.as_ref();
         assert_eq!(
             metrics.waiting.load(std::sync::atomic::Ordering::Acquire),
             0,
         );
-        assert_eq!(
-            metrics.completed.load(std::sync::atomic::Ordering::Acquire),
-            jobs.len() as u64,
-        );
-        jobs.sort_unstable_by(|a, b| a.id.cmp(&b.id).reverse());
         let mut expected_ordered: VecDeque<u64> = jobs
             .into_iter()
             .map(|job| job.id.unwrap_or_default())
             .collect();
-        while let (Some(expected), Some(recieved)) = (expected_ordered.pop_front(), completed.pop())
+        while let (Some(expected), Some(recieved)) =
+            (expected_ordered.pop_back(), moved_to_active.pop())
         {
             assert_eq!(expected, recieved);
         }
+        queue.obliterate().await?;
+        Ok(())
+    }
+    #[tokio::test(flavor = "multi_thread")]
+    async fn runs_jobs_and_respects_clean_up() -> KioResult<()> {
+        use kio_mq::{CollectionSuffix, RemoveOnCompletionOrFailure};
+        let removal_opts = RemoveOnCompletionOrFailure::Bool(true);
+        let config = &CONFIG;
+        let queue_opts = QueueOpts {
+            remove_on_fail: Some(removal_opts),
+            remove_on_complete: Some(removal_opts),
+            ..Default::default()
+        };
+        let name = Uuid::new_v4().to_string();
+        let count = 4;
+        let queue = Queue::<i32, i32, i32>::new(None, &name, config, Some(queue_opts)).await?;
+        let completed: Arc<ArrayQueue<u64>> = Arc::new(ArrayQueue::new(count as usize));
+        let jobs = completed.clone();
+        queue.on_all_events(move |state: kio_mq::EventParameters<i32, i32, i32>| {
+            let completed = jobs.clone();
+            async move {
+                if let EventParameters::Completed {
+                    ref job,
+                    prev_state: _,
+                    result: _,
+                } = state
+                {
+                    let id = job.id.unwrap();
+                    let _ = completed.push(id);
+                }
+                if let EventParameters::Failed {
+                    reason: _,
+                    job_id,
+                    prev_state: _,
+                } = state
+                {
+                    let _ = completed.push(job_id);
+                }
+            }
+        });
+        let job_iterator = (0..count).map(|i| (i.to_string(), None, i));
+        let processor = move |_conn, job: kio_mq::Job<i32, i32, i32>| async move {
+            // fail the 3 third job
+            if job.id.unwrap_or_default() == 3 {
+                panic!("failed here")
+            }
+            Ok::<i32, KioError>(job.data.unwrap())
+        };
+        let worker = Worker::new_async(&queue, processor, None)?;
+        worker.run()?;
+        assert!(worker.is_running());
+        let _jobs = queue.bulk_add(job_iterator).await?;
+        // wait for metrics to update
+        while completed.len() < count as usize {}
+        worker.close(false);
+        let last_id = queue
+            .current_metrics
+            .last_id
+            .load(std::sync::atomic::Ordering::Acquire);
+        let mut pipeline = redis::pipe();
+        pipeline.atomic();
+        let mut conn = queue.get_connection().await?;
+        for id in 1..last_id {
+            let job_key = CollectionSuffix::Job(id).to_collection_name(&queue.prefix, &queue.name);
+            pipeline.exists(job_key);
+        }
+        let all_exist: Vec<bool> = pipeline.query_async(&mut conn).await?;
+        assert!(!all_exist.is_empty());
+        assert!(all_exist.iter().all(|v| !v));
+
         queue.obliterate().await?;
         Ok(())
     }
